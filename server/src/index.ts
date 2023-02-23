06d090ec8239287b4af227f2fba8bc1c93a9b95c
emmanueluwa <emmanuel@fulodev.com>
2023-02-23 18:46:08 +0000

    making change to response.header for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,8 +19,10 @@ import { sendRefreshToken } from "./sendRefreshToken";
     const app = express();
     app.use(function (_req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+            );
         next();
     });
     app.use(cors({
